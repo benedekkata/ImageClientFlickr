3ab7bb05d16969f938a02254d0099ba5385619d2
Benedek Katalin <35570314+benedekkata@users.noreply.github.com>
2022-05-04 19:13:22 +0200

    Comments and refactoring

--- src/app/components/image-search/image-search.component.ts
+++ src/app/components/image-search/image-search.component.ts
@@ -2,8 +2,7 @@ import { FlickrImage } from 'src/app/models/flickrImage.type';
 import { Image } from './../../models/image.type';
 import { Component, OnInit } from '@angular/core';
 import { ImageService } from 'src/app/services/image.service';
-import { FormControl } from '@angular/forms';
-import { POSSIBLE_LANGUAGES } from 'src/app/constants';
+import { BANNED_USER, POSSIBLE_LANGUAGES } from 'src/app/constants';
 import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
@@ -29,28 +28,20 @@ export class ImageSearchComponent implements OnInit {
   selected: string = '';
   languages: string[] = [];
 
+  /**
+   * Calls the imageService.getImagesByKeywordAndLanguage function to get the images for the current user.
+   * If the request is successfull it calls saving functions of the service.
+   */
   getImages() {
     this.imageService
       .getImagesByKeywordAndLanguage(this.keyword, this.selected)
-      .subscribe((images) => {
-        if (images) {
-          const fimages = images as FlickrImage;
+      .subscribe((fimages) => {
+        if (fimages) {
           this.images = fimages.items;
-          this.images.forEach((img: Image) => {
-            //cut and save the last paragraph only
-            const paragraph = /<p>(.*?)<\/p>/gi;
-            const matches = [...img.description.matchAll(paragraph)];
-            if (matches.length !== 0) {
-              img.description = matches[2] ? matches[2][1] : '';
-            }
-            const usernameRegex = /\"(.*?)\"/gi;
-            const usernameMatches = img.author.match(usernameRegex);
+          this.processImages();
+          this.removeBannedUser();
 
-            img.author = usernameMatches ? usernameMatches[0] : '';
-          });
-          this.images = this.images.filter(
-            (image) => image.author_id !== '193744073@N04'
-          );
+          //The request was successful, saving the resonse to the local storeage
           this.storageService.saveLastSearchResult(fimages);
           this.storageService.saveLastKeyword(this.keyword);
           this.storageService.saveLang(this.selected);
@@ -58,8 +49,39 @@ export class ImageSearchComponent implements OnInit {
       });
   }
 
+  /**
+   * Sets the value for the keyword, then reloads the images.
+   * @param newValue the new value for the keyword
+   */
   changeKeyword(newValue: string) {
     this.keyword = newValue;
     this.getImages();
+  }
+
+  /**
+   * Processes the data to be suitable for rendering.
+   */
+  private processImages() {
+    this.images.forEach((img: Image) => {
+      //cut and save the last paragraph only
+      const paragraph = /<p>(.*?)<\/p>/gi;
+      const matches = [...img.description.matchAll(paragraph)];
+      if (matches.length !== 0) {
+        img.description = matches[2] ? matches[2][1] : '';
+      }
+      const usernameRegex = /\"(.*?)\"/gi;
+      const usernameMatches = img.author.match(usernameRegex);
+
+      img.author = usernameMatches ? usernameMatches[0] : '';
+    });
+  }
+
+  /**
+   * Removes BANNED_USER's images (18+ without any flag)
+   */
+  private removeBannedUser() {
+    this.images = this.images.filter(
+      (image) => image.author_id !== BANNED_USER
+    );
   }
 }
--- src/app/components/user-images/user-images.component.ts
+++ src/app/components/user-images/user-images.component.ts
@@ -1,16 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ImageService } from 'src/app/services/image.service';
 import { Image } from './../../models/image.type';
-import { StorageService } from 'src/app/services/storage.service';
-import { FlickrImage } from 'src/app/models/flickrImage.type';
-import {
-  ActivatedRoute,
-  NavigationEnd,
-  NavigationStart,
-  Router,
-  RouterEvent,
-} from '@angular/router';
-import { filter } from 'rxjs/internal/operators/filter';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { BANNED_USER } from 'src/app/constants';
 
 @Component({
   selector: 'app-user-images',
@@ -23,74 +15,86 @@ export class UserImagesComponent implements OnInit {
     private route: ActivatedRoute,
     public router: Router
   ) {
+    //After the route changed (someone cliked a username from the userpage) loads the new id
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
         this.getImgInfo();
+        this.getImages();
       }
     });
   }
 
   ngOnInit(): void {
     this.getImgInfo();
+    this.getImages();
   }
+
   images: Image[] = [];
   uid: string = '';
   username: string = '';
   imgOrFav: string = 'Images';
 
+  /**
+   * Loads the userId and the Username from the URL.
+   */
   getImgInfo() {
     this.uid = this.route.snapshot.params['id'].split(';')[0];
     this.username = this.route.snapshot.params['id'].split(';')[1];
-    this.getImages();
   }
 
+  /**
+   * Calls the imageService.getImagesByUser function to get the images for the current user.
+   */
   getImages() {
     this.imageService.getImagesByUser(this.uid).subscribe((images) => {
       if (images) {
-        const fimages = images as FlickrImage;
-        this.images = fimages.items;
-        this.images.forEach((img: Image) => {
-          //cut and save the last paragraph only
-          const paragraph = /<p>(.*?)<\/p>/gi;
-          const matches = [...img.description.matchAll(paragraph)];
-          if (matches.length !== 0) {
-            img.description = matches[2] ? matches[2][1] : '';
-          }
-          const usernameRegex = /\"(.*?)\"/gi;
-          const usernameMatches = img.author.match(usernameRegex);
-
-          img.author = usernameMatches ? usernameMatches[0] : '';
-        });
-        this.images = this.images.filter(
-          (image) => image.author_id !== '193744073@N04'
-        );
-        this.imgOrFav = 'Images';
+        this.images = images.items;
+        this.processImages();
+        this.removeBannedUser();
       }
     });
+    this.imgOrFav = 'Images';
   }
 
+  /**
+   * Calls the imageService.getFavouritImages function to get the favourite images for the current user.
+   */
   getFavImages() {
     this.imageService.getFavouritImages(this.uid).subscribe((images) => {
       if (images) {
-        const fimages = images as FlickrImage;
-        this.images = fimages.items;
-        this.images.forEach((img: Image) => {
-          //cut and save the last paragraph only
-          const paragraph = /<p>(.*?)<\/p>/gi;
-          const matches = [...img.description.matchAll(paragraph)];
-          if (matches.length !== 0) {
-            img.description = matches[2] ? matches[2][1] : '';
-          }
-          const usernameRegex = /\"(.*?)\"/gi;
-          const usernameMatches = img.author.match(usernameRegex);
-
-          img.author = usernameMatches ? usernameMatches[0] : '';
-        });
-        this.images = this.images.filter(
-          (image) => image.author_id !== '193744073@N04'
-        );
+        this.images = images.items;
+        this.processImages();
+        this.removeBannedUser();
       }
     });
     this.imgOrFav = 'Favourites';
+  }
+
+  /**
+   * Processes the data to be suitable for rendering.
+   */
+  private processImages() {
+    this.images.forEach((img: Image) => {
+      //cut and save the last paragraph of the description field only
+      const paragraph = /<p>(.*?)<\/p>/gi;
+      const matches = [...img.description.matchAll(paragraph)];
+      if (matches.length !== 0) {
+        img.description = matches[2] ? matches[2][1] : '';
+      }
+
+      //cut and save the only the username part of the data (email is not needed)
+      const usernameRegex = /\"(.*?)\"/gi;
+      const usernameMatches = img.author.match(usernameRegex);
+      img.author = usernameMatches ? usernameMatches[0] : '';
+    });
+  }
+
+  /**
+   * Removes BANNED_USER's images (18+ without any flag)
+   */
+  private removeBannedUser() {
+    this.images = this.images.filter(
+      (image) => image.author_id !== BANNED_USER
+    );
   }
 }
